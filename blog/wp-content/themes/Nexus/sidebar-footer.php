343c8be7623f9ccd2485d2cb845e7e83adf57ac4
Acens <projetos@acens.com.br>
2014-02-07 12:47:14 -0300

    coisas

--- blog/wp-content/themes/Nexus/sidebar-footer.php
+++ blog/wp-content/themes/Nexus/sidebar-footer.php
@@ -16,5 +16,22 @@
 			endif;
 		endforeach;
 	?>
+	<div id="contato-footer-1">
+			<h5>QUEM SOMOS</h5>
+		<div class="textwidget-1">
+			<br>Pioneira no Estado, desde 1992, <br>vem proporcionando desenvolvimento <br>profissional e possibilitando elevada <br>satisfação aos seus clientes.
+		</div><!--end #textwidget-1-->
+	</div><!--end #contato-footer-1-->
+	<div id="contato-footer-2">
+			<h5>ATENDIMENTO</h5>
+		<div class="textwidget-2">
+				<br>De Seg à Sex, das 6 às 18 horas
+				<br>Tel|+55 85 3101-9936
+				<br>Avenida Dedé Brasil, 1700
+				Universidade Estadual do Ceará, Bloco CESA, Sala 1		
+		</div><!--end #textwidget-2-->
+		
+	</div><!--end #contato-footer-->
+
 	</div> <!-- #footer-widgets -->
 </footer> <!-- #main-footer -->
